2f66e8c474bdabb90b15b5c2d7c58e132098cb31
Juanchavez93 <juanchavez1191993@gmail.com>
2024-02-01 21:44:22 -0700

    re done for server

--- controllers/image.js
+++ controllers/image.js
@@ -4,26 +4,26 @@ const app = new Clarifai.App({
     apiKey: 'e1b9138af91a4fdaab18a4609e76fb9b'
     });
 
-const handleApiCall = (req, res) => {
-    app.models.predict('face-detection', req.body.input)
-        .then(data => {
-        res.json(data);
+    const handleApiCall = (req, res) => {
+        app.models.predict('face-detection', req.body.input)
+          .then(data => {
+            res.json(data);
+          })
+          .catch(err => res.status(400).json('unable to work with API'))
+      }
+
+      const handleImage = (req, res, postgres) => {
+        const { id } = req.body;
+        postgres('users').where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+          res.json(entries[0].entries);
         })
-            .catch(err => res.status(400).json('unable to work with API'))
-    }
+        .catch(err => res.status(400).json('unable to get entries'))
+      }
 
-const handleImage = (req, res, postgres) => {
-    const { id } = req.body;
-    postgres('users').where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries => {
-        res.json(entries[0].entries);
-    })
-    .catch(err => res.status(400).json('unable to get entries'))
-}
-
-module.exports = {
-handleImage,
-handleApiCall
-}
+      module.exports = {
+        handleImage,
+        handleApiCall
+      }
--- server.js
+++ server.js
@@ -11,28 +11,28 @@ const image = require('./controllers/image');
 
 const postgres = knex({
 
-    client: 'pg',
-    connection: {
-      host : '127.0.0.1',
-      user : '',
-      password : '',
-      database : 'smart-brain'
-    }
-    });
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : '',
+    password : '',
+    database : 'smart-brain'
+  }
+});
 
-    const app = express();
+const app = express();
 
 
-    app.use(cors())
-    app.use(express.json());
+app.use(cors())
+app.use(express.json());
 
-    app.get('/', (req, res)=> { res.send(postgres.users) })
-    app.post('/signin', signin.handleSignin(postgres, bcrypt))
-    app.post('/register', (req, res) => { register.handleRegister(req, res, postgres, bcrypt) })
-    app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, postgres)})
-    app.put('/image', (req, res) => { image.handleImage(req, res, postgres)})
-    app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+app.get('/', (req, res)=> { res.send(postgres.users) })
+app.post('/signin', signin.handleSignin(postgres, bcrypt))
+app.post('/register', (req, res) => { register.handleRegister(req, res, postgres, bcrypt) })
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, postgres)})
+app.put('/image', (req, res) => { image.handleImage(req, res, postgres)})
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-    app.listen(3000, ()=> {
-    console.log('app is running on port 3000');
+app.listen(3000, ()=> {
+  console.log('app is running on port 3000');
 })
